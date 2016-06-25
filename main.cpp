fac3ae325ca1b837c493a882de7551eabaddf549
Rin <Rin@Rin-PC>
2016-06-25 08:25:59 -0500

    Item class added.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-/*
- 
- */
-
 /* 
  * File:   main.cpp
  * Author: Rin
@@ -15,7 +11,10 @@
 #include <string>
 using namespace std;
 
-
+/*
+ *  Class Product contains 3 members containing the id, category, and
+ *  condition of the product
+ */
 class Product{
 protected:
     // Data members
@@ -30,10 +29,41 @@ public:
     virtual ~Product();
     
     // Methods
+    int display();
+    string getId();
+    string getCategory();
+    string getCondition();
 };
+
+// Return the id of the Product
+Product::getId(){
+    return id;
+}
+
+// Return the category of the product
+Product::getCategory(){
+    return category;
+}
+
+// Return the condition of the product
+Product::getCondition(){
+    return condition;
+}
+
+/*
+ * Display information about this product, with each field 
+ * displayed on a new line.
+*/
+Product::display(){
+    cout << "Product id: " << id << "\n" <<
+                "Product category: " << category << "\n" <<
+                "Product condition: " << condition << "\n" << endl;
+    
+}
 
 // Default constructor method
 Product::Product(){
+    // Initialize fields as empty strings
     id = "";
     category = "";
     condition = "";
@@ -41,19 +71,63 @@ Product::Product(){
 
 // Constructor method passed 3 strings
 Product::Product(string i, string cat, string cond){
+    // Set fields based on passed parameters
     id = i;
     category = cat;
     condition = cond;
 }
 
 // Destructor for default constructor
-Product::~Product(){
+Product::~Product(){ }
+
+/*
+ *  Class Item contains 4 members the title, price, availability, and
+ *  color of the object.
+*/
+class Item : public Product{
+protected:
+    // Data members
+    string title;
+    string avail;
+    string color;
+    string price;
+
+public:
+    // Constructors
+    Item();
+    Item(string t, string ava, string col, string pri);
+    virtual ~Item();
+    
+    // Methods
+    int display();
+};
+
+/*
+ * Function to open and read input file. If the file
+ * does not exist, return false to the caller. Otherwise,
+ * return true
+ */
+bool readFile(char fileName){
+    string line; // string to hold one line of the file
+    ifstream myFile ( fileName ); // open the file
+    
+    // 
+    if ( !myFile.is_open() )
+    cout<<"Could not open file\n";
+    else {
+   while ( getline (myFile,line) )
+   {
+   cout << line << '\n';
+   }
+myFile.close();
+}
+ }
 }
 
 int main(int argc, char** argv) {
     
     Product* myproduct = new Product("me", "big men", "brand new");
-    cout << myproduct << endl;
+    myproduct->display();
 
     return 0;
 }
