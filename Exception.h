612b7e73c476b25c88a751c7c935146a2a293ca5
Rin <Rin@10.206.148.112>
2016-06-26 19:56:16 -0500

    Added Exception.h and changed to accept command line args.

---
+++ Exception.h
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Exception.h
+ * Author: Rin
+ *
+ * Created on June 26, 2016, 7:43 PM
+ */
+
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+class Exception { };
+#endif /* EXCEPTION_H */
+
